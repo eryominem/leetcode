94ad08f1f8d900cb1937e11e1a20ca0af1c26c47
eryominem <old.wiss@yandex.ru>
2023-02-28 17:51:51 +0400

    task about binary tree (104, 111)

---
+++ src/MaximumDepthOfBinaryTree_0104/Solution.java
@@ -0,0 +1,30 @@
+package MaximumDepthOfBinaryTree_0104;
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftMaxDepth = maxDepth(root.left);
+        int rightMaxDepth = maxDepth(root.right);
+
+        return leftMaxDepth > rightMaxDepth ? leftMaxDepth + 1 : rightMaxDepth + 1;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {
+    }
+    TreeNode(int val) {
+        this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
