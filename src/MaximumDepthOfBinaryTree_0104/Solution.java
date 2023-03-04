1b31711a1d7fef7322df8e468e4fd3d6567b0ce3
eryominem <old.wiss@yandex.ru>
2023-03-04 15:55:06 +0400

    binary tree

--- src/MaximumDepthOfBinaryTree_0104/Solution.java
+++ src/MaximumDepthOfBinaryTree_0104/Solution.java
@@ -1,7 +1,22 @@
 package MaximumDepthOfBinaryTree_0104;
 
 public class Solution {
-    public int maxDepth(TreeNode root) {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+
+        root.left = new TreeNode(9);
+       /* root.left.left = null;
+        root.left.right = null;*/
+
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        int depth = maxDepth(root);
+        System.out.println(depth);
+    }
+
+    public static int maxDepth(TreeNode root) {
         if (root == null) {
             return 0;
         }
---
+++ src/PathSum_0112/Solution.java
@@ -0,0 +1,32 @@
+package PathSum_0112;
+
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.val == targetSum && root.left == null && root.right == null) {
+            return true;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode() {
+
+    }
+    public TreeNode (int val) {
+        this.val = val;
+    }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
